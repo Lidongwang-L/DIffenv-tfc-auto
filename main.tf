6c4f42adda23af88075a30a658f119b7d390cf0f
Lidongwang GitHub <lidong.wang@linvest21.com>
2023-10-01 11:50:13 -0400

    Production

--- main.tf
+++ main.tf
@@ -47,9 +47,12 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello new-pppp" > /var/www/html/index.html
+              echo "Hello Production" > /var/www/html/index.html
               systemctl restart apache2
               EOF
+}
+tags = {
+  Name = "Production Env"
 }
 
 resource "aws_security_group" "web-sg" {
