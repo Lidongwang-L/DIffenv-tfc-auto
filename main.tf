25ae0c72a89ef3854c00bd9f1129122ade2e4cde
Lidongwang GitHub <lidong.wang@linvest21.com>
2023-10-01 21:15:10 -0400

    test for 2024

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello add dft" > /var/www/html/index.html
+              echo "Hello 2024" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
