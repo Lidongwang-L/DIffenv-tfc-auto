5c957b96ea142a3c3d6bd147b7cbcc9120898cb6
Lidongwang GitHub <lidong.wang@linvest21.com>
2023-10-04 09:43:35 -0400

    prod

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ provider "aws" {
   region = "us-west-2"
   default_tags {
     tags = {
-      Env = "pro"
+      Env = "prod"
     }
   }
 }
@@ -52,7 +52,7 @@ resource "aws_instance" "web" {
               apt-get update
               apt-get install -y apache2
               sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "Hello 00--" > /var/www/html/index.html
+              echo "Hello prod" > /var/www/html/index.html
               systemctl restart apache2
               EOF
 }
